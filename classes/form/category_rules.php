99218a443914dc03d8654b652a20fc1cf0095d06
Skylar Kelty <s.kelty@kent.ac.uk>
2015-08-24 17:00:08 +0100

    Implement get_rules first pass

--- classes/form/category_rules.php
+++ classes/form/category_rules.php
@@ -102,16 +102,28 @@ class category_rules extends \moodleform
     private function add_rule_fieldset($obj = null) {
         $mform =& $this->_form;
 
-        // Where are we?
+        // Extract some object info.
+        $id = isset($obj) ? "rule_{$obj->id}_" : 'rule_new_';
         $data = isset($obj) ? unserialize($obj->data) : null;
-        $rule = isset($obj) ? $obj->rule : optional_param('rule', '', PARAM_ALPHANUMEXT);
-        $target = isset($obj) ? $obj->target : optional_param('target', '', PARAM_ALPHANUMEXT);
-        $datatype = isset($obj) ? $obj->datatype : optional_param('datatype', '', PARAM_ALPHANUMEXT);
-        $activity = is_array($data) && isset($data['activity']) ? $data['activity'] : optional_param('activity', '', PARAM_ALPHANUMEXT);
+
+        // Where are we?
+        //  - If we are new, default is '' otherwise the default is the object's value
+        //    and we take the parameter as primary.
+        $defaultrule = isset($obj) ? $obj->rule : '';
+        $rule = optional_param($id . 'rule', $defaultrule, PARAM_ALPHANUMEXT);
+
+        $defaulttarget = isset($obj) ? $obj->target : '';
+        $target = optional_param($id . 'target', $defaulttarget, PARAM_ALPHANUMEXT);
+
+        $defaultdatatype = isset($obj) ? $obj->datatype : '';
+        $datatype = optional_param($id . 'datatype', $defaultdatatype, PARAM_ALPHANUMEXT);
+
+        $defaultactivity = is_array($data) && isset($data['activity']) ? $data['activity'] : '';
+        $activity = optional_param($id . 'activity', $defaultactivity, PARAM_ALPHANUMEXT);
 
         // Add a selection box for the rule.
         $validrules = \tool_cat\external\rule::get_rules();
-        $mform->addElement('select', 'rule', 'Rule', array_merge(array(0 => 'Select a rule'), $validrules));
+        $mform->addElement('select', $id . 'rule', 'Rule', array_merge(array(0 => 'Select a rule'), $validrules));
 
         // Do we have a rule?
         if (empty($rule)) {
@@ -120,9 +132,9 @@ class category_rules extends \moodleform
 
         // We have a rule!
         $validtargets = \tool_cat\external\rule::get_targets($rule);
-        $mform->addElement('select', 'target', 'Target', array_merge(array(0 => 'Select a target'), $validtargets));
-        $mform->addElement('text', 'targetid', 'Target Identifier');
-        $mform->setType('targetid', PARAM_TEXT);
+        $mform->addElement('select', $id . 'target', 'Target', array_merge(array(0 => 'Select a target'), $validtargets));
+        $mform->addElement('text', $id . 'targetid', 'Target Identifier');
+        $mform->setType($id . 'targetid', PARAM_TEXT);
 
         // Do we have a target?
         if (empty($target)) {
@@ -131,7 +143,7 @@ class category_rules extends \moodleform
 
         // We have a target!
         $validdatatypes = \tool_cat\external\rule::get_datatypes($target);
-        $mform->addElement('select', 'datatype', 'Data type', array_merge(array(0 => 'Select a data type'), $validdatatypes));
+        $mform->addElement('select', $id . 'datatype', 'Data type', array_merge(array(0 => 'Select a data type'), $validdatatypes));
 
         // Do we have a datatype?
         if (empty($datatype)) {
@@ -142,7 +154,7 @@ class category_rules extends \moodleform
         switch ($datatype) {
             case 'activity':
                 $validactivities = \tool_cat\external\rule::get_activities();
-                $mform->addElement('select', 'activity', 'Activity', array_merge(array(0 => 'Select an activity'), $validactivities));
+                $mform->addElement('select', $id . 'activity', 'Activity', array_merge(array(0 => 'Select an activity'), $validactivities));
 
                 // Do we have an activity submitted?
                 if (empty($activity)) {
@@ -152,50 +164,50 @@ class category_rules extends \moodleform
                 // Add fields.
                 $validfields = \tool_cat\external\rule::get_activity_fields($activity);
                 foreach ($validfields as $name => $type) {
-                    $mform->addElement('text', $name, ucwords($name));
-                    $mform->setType($name, $type);
+                    $mform->addElement('text', $id . $name, ucwords($name));
+                    $mform->setType($id . $name, $type);
 
                     if (isset($obj) && isset($data[$name])) {
-                        $mform->setDefault($name, $data[$name]);
+                        $mform->setDefault($id . $name, $data[$name]);
                     }
                 }
             break;
 
             case 'block':
                 $validblocks = \tool_cat\external\rule::get_blocks();
-                $mform->addElement('select', 'block', 'Block', array_merge(array(0 => 'Select a block'), $validblocks));
+                $mform->addElement('select', $id . 'block', 'Block', array_merge(array(0 => 'Select a block'), $validblocks));
 
-                $mform->addElement('select', 'blockpos', 'Block Position', array(
+                $mform->addElement('select', $id . 'blockpos', 'Block Position', array(
                     \BLOCK_POS_LEFT => 'Left',
                     \BLOCK_POS_RIGHT => 'Right'
                 ));
 
                 if (isset($obj)) {
-                    $mform->setDefault('block', $data['block']);
-                    $mform->setDefault('blockpos', $data['blockpos']);
+                    $mform->setDefault($id . 'block', $data['block']);
+                    $mform->setDefault($id . 'blockpos', $data['blockpos']);
                 }
             break;
 
             case 'section':
-                $mform->addElement('text', 'title', 'Section title');
-                $mform->setType('title', PARAM_TEXT);
+                $mform->addElement('text', $id . 'title', 'Section title');
+                $mform->setType($id . 'title', PARAM_TEXT);
 
-                $mform->addElement('textarea', 'summary', 'Section summary');
-                $mform->setType('summary', PARAM_TEXT);
+                $mform->addElement('textarea', $id . 'summary', 'Section summary');
+                $mform->setType($id . 'summary', PARAM_TEXT);
 
                 if (isset($obj)) {
-                    $mform->setDefault('title', $data['title']);
-                    $mform->setDefault('summary', $data['summary']);
+                    $mform->setDefault($id . 'title', $data['title']);
+                    $mform->setDefault($id . 'summary', $data['summary']);
                 }
             break;
 
             case 'text':
             case 'template':
-                $mform->addElement('textarea', 'data', 'Data');
-                $mform->setType('data', PARAM_TEXT);
+                $mform->addElement('textarea', $id . 'data', 'Data');
+                $mform->setType($id . 'data', PARAM_TEXT);
 
                 if (isset($obj)) {
-                    $mform->setDefault('data', $data);
+                    $mform->setDefault($id . 'data', $data);
                 }
             break;
 
@@ -205,18 +217,31 @@ class category_rules extends \moodleform
         }
 
         if (isset($obj)) {
-            $mform->setDefault('rule', $obj->rule);
-            $mform->setDefault('target', $obj->target);
-            $mform->setDefault('targetid', $obj->targetid);
-            $mform->setDefault('datatype', $obj->datatype);
+            $mform->setDefault($id . 'rule', $obj->rule);
+            $mform->setDefault($id . 'target', $obj->target);
+            $mform->setDefault($id . 'targetid', $obj->targetid);
+            $mform->setDefault($id . 'datatype', $obj->datatype);
         }
     }
 
     /**
      * Returns rule data.
+     *
+     * @param array $data Form data.
      */
-    public function get_rules() {
-        // TODO.
+    public function get_rules($data) {
+        $rules = array();
+
+        foreach ((array)$data as $k => $v) {
+            list($rule, $id, $name) = explode('_', $k);
+            if (!isset($rules[$id])) {
+                $rules[$id] = array();
+            }
+
+            $rules[$id][$name] = $v;
+        }
+
+        return $rules;
     }
 
     /**
