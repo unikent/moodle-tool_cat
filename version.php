0a59c9d058f089d8cf5b6d2ef094ac2e13305339
Skylar Kelty <s.kelty@kent.ac.uk>
2015-12-11 13:44:18 +0000

    3.0 compatibility

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_cat';
-$plugin->version   = 2015110300;
-$plugin->requires  = 2014051200;
+$plugin->version   = 2015121200;
+$plugin->requires  = 2015111600;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0 (Build: 2015110300)';
+$plugin->release = '1.1 (Build: 2015121200)';
